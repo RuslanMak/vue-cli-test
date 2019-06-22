6cef7ca24df30280fdd0d2ab386c36be474fdd4f
RuslanMak <makarovemd@gmail.com>
2019-06-22 15:32:49 +0300

    done SPA

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins {{count}}</h3>
+    <button v-on:click="addOne()">add 1</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  // data() {
+  //   return {
+  //     count: 1
+  //   }
+  // },
+  // or стрелачная ф-ция
+  data: () => ({
+    count: 1
+  }),
+
+  methods: {
+    addOne() {
+      this.count++;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
